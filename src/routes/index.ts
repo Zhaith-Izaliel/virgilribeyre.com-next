a7a9d4b5566c5e332f8dbb6b6f5af843cd62a48e
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-02-20 14:32:54 +0100

    feat: add 404 page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,13 @@
 import HomePage from '@/pages/home/Home.vue';
+import NotFoundPage from '@/pages/not-found/NotFound.vue';
 
 export default [
   {
     path: '/',
     component: HomePage,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: NotFoundPage,
   },
 ];
